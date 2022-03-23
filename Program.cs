c4266c06fe85d8a89d8c53f43464dd173095bada
14ibnurmy <rokhym140103@gmail.com>
2022-03-23 21:06:41 +0700

    Taxi

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Taxi
+{
+    static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Taxi taxi = new();
+            taxi.DriverName = "Romy";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 3;
+            taxi.TaxiInfo();
+            Console.WriteLine();
+            taxi.PickupPassenger();
+            taxi.DropOffPassenger();
+            Console.ReadKey();
+        }
+    }
+}
